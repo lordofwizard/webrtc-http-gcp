603d1219d3d003164d6c4ec085e5cd72bfc0d870
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-06-06 12:59:26 +0530

    [SETUP] basic file structure and setup done

---
+++ signaling.py
@@ -0,0 +1,7 @@
+# This is the http signaling server that will send request
+
+# Some routes
+# /robot -> sends 
+# /video_device -> sends it's own sdp and grabs the client sdp.
+# /robot_check -> checks every one second for response of client sdp. 
+
